df75414291a00ed84d4d9670efdc19f37095581e
Renan Volpe <renanvolpe@hotmail.com>
2019-05-28 22:56:40 -0300

    Adicionando uma função script para adicionar um valor na lista sem imagem, modificando a pagina home para sugestão de adicionar comentario e bordar preta nas caixas de noticias

--- jcript/scriptProjeto.js
+++ jcript/scriptProjeto.js
@@ -1,9 +1,10 @@
 function adicionarNoticia() {
 	var noticia = document.getElementById("noticia").value;
-
+	
 	var lista = document.getElementById("lista").innerHTML;
 
-	lista = lista +"<li>"+noticia+"</li>";
+	lista =  lista +'<li>'+noticia+'</li>';
 
 	document.getElementById("lista").innerHTML = lista;
 }
+
