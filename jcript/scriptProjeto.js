4321d77f1a625b8fb9e3ae253423202437929553
Renan Volpe <renanvolpe@hotmail.com>
2019-06-05 02:23:46 -0300

    tentando inserir imagem por script

--- jcript/scriptProjeto.js
+++ jcript/scriptProjeto.js
@@ -3,7 +3,16 @@ function adicionarNoticia() {
 	
 	var lista = document.getElementById("lista").innerHTML;
 
-	lista =  lista +'<li>'+noticia+'</li>';
+	
+
+	
+
+	 var src = document.getElementById("noticia");
+     var img = document.createElement("img");
+     img.src = '../imagem/ImagemTeste.jpg';
+     var image = src.appendChild(img);
+
+     lista =  lista +'<li>'+noticia+image+'</li>';
 
 	document.getElementById("lista").innerHTML = lista;
 }
