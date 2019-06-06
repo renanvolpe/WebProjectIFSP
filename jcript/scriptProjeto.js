0ba1bbbc0c70faf34ba056011d4535f41e74f86e
Renan Volpe <renanvolpe@hotmail.com>
2019-06-06 18:13:24 -0300

    Colocando Resposividade no site para meia dela(nao celular), arrumando menu que nao tava no topo. Colocando javaScript de htmlProjeto para ScriptProjeto para que, quando a janela carregar, ele atualize e faça a função

--- jcript/scriptProjeto.js
+++ jcript/scriptProjeto.js
@@ -17,3 +17,44 @@ function adicionarNoticia() {
 	document.getElementById("lista").innerHTML = lista;
 }
 
+function Menufixo(){
+	var nav =document.getElementById('nav');
+	var  header = document.getElementById('header');
+
+					window.onscroll = function() {
+						if (window.pageYOffset >200 ) {
+							nav.style.transition = ".3s";
+							nav.style.background = "black";
+							nav.style.boxShadow ="0px 4px 2px gray";
+							nav.style.position = "absolute";
+							nav.style.position = "fixed";
+							nav.style.top = "0px";
+							nav.style.left = "0px";
+							nav.style.right = "0px";
+							nav.style.width="100%";
+							nav.style.margin="0px";
+							header.style.padding = "0px";
+
+						}
+						else{
+							nav.style.background = "white";
+							nav.style.boxShadow ="none";
+							nav.style.position = "static";
+							nav.style.width="auto";
+							nav.style.margin="60px 60px 60px 0";
+							nav.style.transition = ".0s";
+
+							var tamanhoTela = window.innerWidth;
+
+							if (tamanhoTela > 1200)							
+								header.style.padding="0px 300px";
+							else			
+								header.style.padding="0px";
+							
+							
+							
+
+						}
+					}
+
+}
