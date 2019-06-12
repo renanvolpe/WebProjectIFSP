5cf061aefaef96ad568b6529ba2d74feaa1bff7d
Renan Volpe <renanvolpe@hotmail.com>
2019-06-11 23:35:22 -0300

    Arrumando NavBar para cima, e o newsLetter. Coloquei o navBar no meio quando da o scroll

--- jcript/scriptProjeto.js
+++ jcript/scriptProjeto.js
@@ -27,13 +27,12 @@ function adicionarNoticia() {
 function Menufixo(){
 	var nav =document.getElementById('nav');
 	var  header = document.getElementById('header');
-
+	var menu = document.getElementById('menuzinho');
 					window.onscroll = function() {
 						if (window.pageYOffset >200 ) {
 							nav.style.transition = ".3s";
 							nav.style.background = "black";
 							nav.style.boxShadow ="0px 4px 2px gray";
-							nav.style.position = "absolute";
 							nav.style.position = "fixed";
 							nav.style.top = "0px";
 							nav.style.left = "0px";
@@ -42,18 +41,27 @@ function Menufixo(){
 							nav.style.margin="0px";
 							header.style.padding = "0px";
 
+							var tamanhoTela01 = window.innerWidth;
+
+							if (tamanhoTela01 > 1500)							
+								menuzinho.style.padding="0px 0 0 700px";
+							else			
+								menuzinho.style.padding="0 0 0 250px";
+
+
 						}
 						else{
 							nav.style.background = "white";
 							nav.style.boxShadow ="none";
 							nav.style.position = "static";
 							nav.style.width="auto";
+							menuzinho.style.padding = "0px";
 							nav.style.margin="60px 60px 60px 0";
 							nav.style.transition = ".0s";
 
-							var tamanhoTela = window.innerWidth;
+							var tamanhoTela02 = window.innerWidth;
 
-							if (tamanhoTela > 1200)							
+							if (tamanhoTela02 > 1200)							
 								header.style.padding="0px 300px";
 							else			
 								header.style.padding="0px";
